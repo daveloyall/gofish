bee045da4b72026fb14b589b00c0336fbbefbcc0
smaclennan <smaclennan>
2002-08-25 01:50:08 +0000

    New files for configure.

---
+++ acconfig.h
@@ -0,0 +1,11 @@
+/* Name of package.  */
+#undef PACKAGE
+
+/* Version of package.  */
+#undef VERSION
+
+/* http gateway support */
+#undef USE_HTTP
+
+/* Linux style sendfile */
+#undef HAVE_LINUX_SENDFILE
